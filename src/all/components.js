0fceebe8c3edb371e9bd2ebece2fd540bd08f854
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2016-12-08 17:32:04 -0800

    Remove reference in components.js

--- src/all/components.js
+++ src/all/components.js
@@ -5,7 +5,6 @@ import SSGReadingProgress from '../SSGReadingProgress';
 import SSGSearch from '../SSGSearch';
 import SSGSearchAutocomplete from '../SSGSearchAutocomplete';
 import SSGSearchBase from '../SSGSearchBase';
-import SSGSidebar from '../SSGSidebar';
 import SSGUpdates from '../SSGUpdates';
 
 export {
@@ -14,7 +13,6 @@ export {
 	SSGSearch,
 	SSGSearchAutocomplete,
 	SSGSearchBase,
-	SSGSidebar,
 	SSGUpdates
 };
 
