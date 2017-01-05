2896d3da9dd450cf22f75b79180a99135c1341b9
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-01-05 13:03:05 -0800

    Rename

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var metal = require('gulp-metal');
 
 metal.registerTasks({
-	bundleCssFileName: 'ssg-components.css',
-	bundleFileName: 'ssg-components.js',
-	moduleName: 'metal-ssg-components'
+	bundleCssFileName: 'electric-components.css',
+	bundleFileName: 'electric-components.js',
+	moduleName: 'electric-components'
 });
