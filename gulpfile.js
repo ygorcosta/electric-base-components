4d6cf2182e5dbc10663c3fac5d28a76bb70c44b4
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-02-09 10:12:39 -0800

    Remove unnecessary dependencies

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var metal = require('gulp-metal');
-
-metal.registerTasks({
-	bundleCssFileName: 'electric-components.css',
-	bundleFileName: 'electric-components.js',
-	moduleName: 'electric-components'
-});
